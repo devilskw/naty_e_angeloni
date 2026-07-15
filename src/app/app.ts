53395de2faaf6383e0ab52634831d8bfe854811b
devilskw <kazuo_w@hotmail.com>
2026-07-14 23:22:35 -0300

    feat(casamento): Atualizacoes a pedido casal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, HostListener, OnInit, ChangeDetectorRef, ViewChild  } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { NgIf } from '@angular/common';
 
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 
@@ -16,8 +17,40 @@ import { MatIconModule } from '@angular/material/icon';
     , MatSidenavModule
     , MatListModule
     , MatIconModule
+    , NgIf
   ],
 })
 export class App {
   protected readonly title = signal('weddingday');
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  deviceWidth!: number;
+  opened: boolean = true;
+
+  constructor(private cdr: ChangeDetectorRef ) {}
+
+  isMobile() {
+    return this.deviceWidth <= 500;
+  }
+
+  ngOnInit() {
+    this.deviceWidth = window.innerWidth;
+    this.opened = true;
+    this.cdr.detectChanges();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.deviceWidth = event.target.innerWidth;
+    this.cdr.detectChanges();
+  }
+
+  toggleSidenav() {
+    if (this.isMobile()) {
+      this.sidenav.toggle();
+      this.opened = !this.opened;
+    }
+    this.cdr.detectChanges();
+  }
+
 }
+
